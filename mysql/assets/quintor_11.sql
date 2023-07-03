d2a8a64f0513f6b1f28fd8ad43b919c72ff8a03d
Stefan <stefan.meijer3@student.nhlstenden.com>
2023-07-03 11:43:30 +0200

    small change to sql

--- mysql/assets/quintor_11.sql
+++ mysql/assets/quintor_11.sql
@@ -7,6 +7,7 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
+DROP DATABASE IF EXISTS `quintor`;
 CREATE DATABASE `quintor` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `quintor`;
 
